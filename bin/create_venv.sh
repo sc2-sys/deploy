1beebac9548e49438e307f09a91e762ffacab762
Carlos Segarra <carlos@carlossegarra.com>
2025-02-19 18:33:20 +0000

    azure: install the patched qemu

--- bin/create_venv.sh
+++ bin/create_venv.sh
@@ -6,7 +6,6 @@ THIS_DIR=$(dirname $(readlink -f $0))
 PROJ_ROOT=${THIS_DIR}/..
 VENV_PATH="${PROJ_ROOT}/venv"
 
-PYTHON=python3.10
 PIP=${VENV_PATH}/bin/pip3
 
 function pip_cmd {
@@ -16,7 +15,7 @@ function pip_cmd {
 pushd ${PROJ_ROOT} >> /dev/null
 
 if [ ! -d ${VENV_PATH} ]; then
-    ${PYTHON} -m venv ${VENV_PATH}
+    python3 -m venv ${VENV_PATH}
 fi
 
 pip_cmd install -U pip setuptools wheel
