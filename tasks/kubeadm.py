03cc3e2e65b589995c3276efac74cb284305a803
yoshiisato <yoshis.ny@gmail.com>
2025-04-13 09:43:21 +0900

    added kubeadm sudo reset

--- tasks/kubeadm.py
+++ tasks/kubeadm.py
@@ -30,6 +30,10 @@ def create(debug=False):
 
     print_dotted_line(f"Creating K8s (v{K8S_VERSION}) cluster using kubeadm")
 
+    # Resetting kubeadm
+    print("......reseting kubeadm......")
+    run("sudo kubeadm reset -f")
+
     # Start the cluster
     kubeadm_cmd = "sudo -E kubeadm init --config {}".format(K8S_ADMIN_FILE) # added -E flag to perserve env var
     if debug:
