a4ec87a3b45eb0ffd3934c0f46ac2548e3a67499
yoshiisato <yoshis.ny@gmail.com>
2025-04-14 10:52:26 +0900

    defaulted and standardized proxy config 12 (k8s cluster again) deleted os environ

--- tasks/kubeadm.py
+++ tasks/kubeadm.py
@@ -21,17 +21,10 @@ def create(debug=False):
     """
     Create a single-node k8s cluster
     """
-
-    import os
-    # Inject proxy-related environment variables (for tdx lab node)
-    os.environ['HTTP_PROXY'] = "http://133.9.80.129:3128"
-    os.environ['HTTPS_PROXY'] = "http://133.9.80.129:3128"
-    os.environ['NO_PROXY'] = "127.0.0.1,localhost,10.96.0.0/12,192.168.0.0/16" 
-
     print_dotted_line(f"Creating K8s (v{K8S_VERSION}) cluster using kubeadm")
 
     # Start the cluster
-    kubeadm_cmd = "sudo -E kubeadm init --config {}".format(K8S_ADMIN_FILE) # added -E flag to perserve env var
+    kubeadm_cmd = "sudo kubeadm init --config {}".format(K8S_ADMIN_FILE)
     if debug:
         run(kubeadm_cmd, shell=True, check=True)
     else:
